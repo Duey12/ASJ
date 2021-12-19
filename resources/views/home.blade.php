bda70ef67101762b599dfb3da1f9ccc6f4325c09
Duey12 <duwaine.gisf1@gmail.com>
2021-12-19 09:16:13 -0500

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="index.css" href="/css/master.css">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <script type="text/javascript" src="asj.js"></script>
+  </body>
+</html>
